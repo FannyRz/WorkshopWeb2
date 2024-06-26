a80965e26ea88c2850609871c39c2368950229cc
FannyRz <fanny.rz.celia@gmail.com>
2024-06-26 12:55:42 +0200

    verif mdp

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import model
 app = Flask(__name__)
 CORS(app)
 
-
+pseudo_actif = ""
 
 @app.route("/")
 def accueil():
@@ -14,33 +14,39 @@ def accueil():
 
 @app.route("/connexion", methods=['GET','POST'])
 def connexion():
-    if (request.method == 'POST'):
-        nom_res = request.form['nom']
-        prenom_res = request.form['prenom']
-        naissance_res = request.form['naissance']
-        nationalite_res = request.form['nationalite']
-        pseudo_res = request.form['pseudo']
-
-        #enregistrer un mot de passe hasher 
-        password_reshash = model.hash_psw(request.form['password'])
-        model.form_info(nom_res,prenom_res,naissances_res,nationalite_res,pseudo_res,password_reshash)
-        return render_template("connexion.html")
-
-    if (request.method == 'GET'):
-        return render_template("connexion.html")
     # if (request.method == 'POST'):
+    #     nom_res = request.form['nom']
+    #     prenom_res = request.form['prenom']
+    #     naissance_res = request.form['naissance']
+    #     nationalite_res = request.form['nationalite']
     #     pseudo_res = request.form['pseudo']
 
     #     #enregistrer un mot de passe hasher 
     #     password_reshash = model.hash_psw(request.form['password'])
     #     model.form_info(nom_res,prenom_res,naissances_res,nationalite_res,pseudo_res,password_reshash)
+    #     return render_template("connexion.html")
+
+    if (request.method == 'POST'):
+        pseudo_actif = request.form['pseudo']
+
+        #enregistrer un mot de passe hasher 
+        password_reshash = model.hash_psw(request.form['password'])
+        joueur_bdd = model.get_data("SELECT * FROM JOUEUR WHERE pseudo=%s", (pseudo_actif))
+        if(password_reshash == joueur_bdd.mot_de_passe){
+            return render_template("profil.html")
+        }
+        return render_template("connexion.html")
+        
+
+    return render_template("connexion.html")
 
 @app.route("/profil", methods=['GET','POST'])
 def profil():
-    return render_template("profil.html")
+    return render_template("profil.html", nom = pseudo_actif)
 
 @app.route("/inscription", methods=['GET','POST'])
 def inscription():
+
     return render_template("connexion.html")
 
 @app.route("/sudoku")
